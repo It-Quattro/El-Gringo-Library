1ad26694bc4ab100f18cc833070077b173e8fccb
Nicolas <guigue.nico@gmail.com>
2017-11-23 13:11:25 +0000

    Replace space indent by tabulation

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -14,90 +14,90 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class BooksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-      if (Auth::check()) {
-        $books = DB::table('books')->distinct()->get();
-        return view('library/books', [
-            'booksBDD' => $books
-        ]);
-      }else{
-        return view('welcome');
-      }
+		/**
+		 * Display a listing of the resource.
+		 *
+		 * @return \Illuminate\Http\Response
+		 */
+		public function index()
+		{
+			if (Auth::check()) {
+				$books = DB::table('books')->distinct()->get();
+				return view('library/books', [
+						'booksBDD' => $books
+				]);
+			}else{
+				return view('welcome');
+			}
 
-      
-    }
+			
+		}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-      if($_POST['name'] != "" && $_POST['description'] != "" &&  $_POST['author'] != ""){
-        if($_POST['available'] == "true" || $_POST['available'] == "1"){
-          Books::create([
-            'name' => $_POST['name'],
-            'description' =>  $_POST['description'],
-            'author' =>  $_POST['author'],
-            'available' =>  true,
-          ]);
-        }else if($_POST['available'] == "false" || $_POST['available'] == "0"){
-          Books::create([
-            'name' => $_POST['name'],
-            'description' =>  $_POST['description'],
-            'author' =>  $_POST['author'],
-            'available' =>  false,
-          ]);
-        }
-      }
+		/**
+		 * Show the form for creating a new resource.
+		 *
+		 * @return \Illuminate\Http\Response
+		 */
+		public function create()
+		{
+			if($_POST['name'] != "" && $_POST['description'] != "" &&  $_POST['author'] != ""){
+				if($_POST['available'] == "true" || $_POST['available'] == "1"){
+					Books::create([
+						'name' => $_POST['name'],
+						'description' =>  $_POST['description'],
+						'author' =>  $_POST['author'],
+						'available' =>  true,
+					]);
+				}else if($_POST['available'] == "false" || $_POST['available'] == "0"){
+					Books::create([
+						'name' => $_POST['name'],
+						'description' =>  $_POST['description'],
+						'author' =>  $_POST['author'],
+						'available' =>  false,
+					]);
+				}
+			}
 
-      $books = DB::table('books')->distinct()->get();
+			$books = DB::table('books')->distinct()->get();
 
-      return view('library/books', [
-          'booksBDD' => $books
-      ]);
-    }
+			return view('library/books', [
+					'booksBDD' => $books
+			]);
+		}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update($id)
-    {
-      var_dump($_POST);
-      die;
-      DB::table('books')->where('id', $book)->update();
-      $books = DB::table('books')->distinct()->get();
+		/**
+		 * Update the specified resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request  $request
+		 * @param  int  $id
+		 * @return \Illuminate\Http\Response
+		 */
+		public function update($id)
+		{
+			var_dump($_POST);
+			die;
+			DB::table('books')->where('id', $book)->update();
+			$books = DB::table('books')->distinct()->get();
 
-      return view('books', [
-          'booksBDD' => $books
-      ]);
-    }
+			return view('books', [
+					'booksBDD' => $books
+			]);
+		}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($book)
-    {
-      DB::table('books')->where('id', $book)->delete();
-      $books = DB::table('books')->distinct()->get();
+		/**
+		 * Remove the specified resource from storage.
+		 *
+		 * @param  int  $id
+		 * @return \Illuminate\Http\Response
+		 */
+		public function destroy($book)
+		{
+			DB::table('books')->where('id', $book)->delete();
+			$books = DB::table('books')->distinct()->get();
 
-      return view('library/books', [
-          'booksBDD' => $books
-      ]);
+			return view('library/books', [
+					'booksBDD' => $books
+			]);
 
-    }
+		}
 }
